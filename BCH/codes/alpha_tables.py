babc484ad0c1076af37c24efb10f08969a128471
gadepall <gadepall@gmail.com>
2019-02-15 14:48:55 +0530

    Add existing files

--- BCH/codes/alpha_tables.py
+++ BCH/codes/alpha_tables.py
@@ -6,43 +6,15 @@ Created on Wed Jan 30 00:38:01 2019
 """
 
 import numpy as np
-from generator_polynomial import gen_poly
-import kbch_nbch_selection
 
-#code_rate=np.divide(1,4.0)
-#N=16200
-
-def alpha_lookup_tables(code_rate,N):
-    g,Gg=gen_poly(N)
-    kbch,nbch=kbch_nbch_selection.K_N(code_rate,N)
-    m1,m=Gg.shape
-    m=m-1
-    n_bch=nbch  #(2**(m))-1
-    alpha=np.zeros((n_bch,m),dtype=int)
-    alpha[1:m+1,:]=np.identity(m)
-    for i in range(m+1,n_bch):
-        alpha[i,:]=np.mod((alpha[i-m,:]+alpha[i-m+1,:]),2) 
+def alpha_lookup_tables(t):
+    n_bch=(2**(t))-1
+    alpha=np.zeros((n_bch,t),dtype=int)
+    alpha[1:t+1,:]=np.identity(t)
+    for i in range(t+1,n_bch):
+        alpha[i,:]=np.mod((alpha[i-t,:]+alpha[i-t+1,:]),2)
     return alpha
-    
-#alpha=alpha_lookup_tables(code_rate,N)
-#    
-#kbch,nbch=kbch_nbch_selection.K_N(code_rate,N)  
-#g,Gg=gen_poly(N)  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#mm=14
-#alpha=np.zeros((nbch,mm),dtype=int)
-#alpha[1:mm+1,:]=np.identity(mm)
-#for i in range(mm+1,nbch):
-#    alpha[i,:]=(alpha[i-mm,:]+alpha[i-mm+1,:])%2    
+
+t = 14
+alpha=alpha_lookup_tables(t)
+print(np.shape(alpha))
